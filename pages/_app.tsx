0da5fbe48121e5a0b625409a78aff4fb98bd627e
MoonNight <k0937477331@gmail.com>
2022-10-07 23:53:36 +0800

    feat:Modify icon and meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,19 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Footer from '../components/footer';
 
+const title: string = "Cheng Cheng-Wen Personal Website";
+
+const description: string =
+    "I'm Kevin, I've been studying programming since I was in high school," +
+    "and I've participated some programming competitions and gotten some good results," +
+    "now I'm working in lab.";
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>Cheng Cheng-Wen Personal Website</title>
-                <meta name="description" content="" />
+                <title>{title}</title>
+                <meta name="description" content={description} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Component {...pageProps} />
